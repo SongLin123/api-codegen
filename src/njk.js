99571512145cb71ba00d0fd21d987196d9378310
songlin <634059453@qq.com>
2021-01-20 13:01:07 +0800

    fix:多路由层级的base path

--- src/njk.js
+++ src/njk.js
@@ -1,55 +1,82 @@
 /*
  * @Date: 2020-06-11 16:59:40
- * @LastEditors: songlin
- * @LastEditTime: 2020-06-23 08:41:18
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-09-25 11:46:59
  * @FilePath: \api-codegen\src\njk.js
  */
-import * as nunjucks from "nunjucks"
-import * as path from "path"
-import { writeFile, convertPath } from "./utils"
-import { isNumber } from "lodash";
+import * as nunjucks from 'nunjucks'
+import * as path from 'path'
+import { writeFile, convertPath } from './utils'
+import { isNumber } from 'lodash'
 
-export function createNjk(searchPaths, { autoescape = true, noCache = false, watch = false, throwOnUndefined = false }) {
-    const env = new nunjucks.Environment(
-        new nunjucks.FileSystemLoader(path.resolve(__dirname, searchPaths), {
-            noCache: noCache,
-            watch: watch,
-        }), {
-        autoescape: autoescape,
-        throwOnUndefined: throwOnUndefined
-    })
-    env.addFilter('render', function (template, ctx) {
-        try {
-            return env.filters.safe(env.render(template, ctx))
-        } catch (err) {
-            return err.message
-        }
-    })
-    env.addFilter('OR', function (tar, res, or) {
-        try {
-            return tar ? res : or
-        } catch (err) {
-            return err.message
-        }
-    })
-    env.addFilter('replace', function (tar) {
-        try {
-            if (isNumber(tar.charAt(0))) {
-                tar = '_' + tar
-            }
-            tar = tar.split(/\-/g).join('_')
-            return tar
-        } catch (err) {
-            return err.message
-        }
-    })
-    return env
+export function createNjk (
+  searchPaths,
+  {
+    autoescape = true,
+    noCache = false,
+    watch = false,
+    throwOnUndefined = false
+  }
+) {
+  const env = new nunjucks.Environment(
+    new nunjucks.FileSystemLoader(path.resolve(__dirname, searchPaths), {
+      noCache: noCache,
+      watch: watch
+    }),
+    {
+      autoescape: autoescape,
+      throwOnUndefined: throwOnUndefined
+    }
+  )
+  env.addFilter('render', function (template, ctx) {
+    try {
+      return env.filters.safe(env.render(template, ctx))
+    } catch (err) {
+      return err.message
+    }
+  })
+  env.addFilter('OR', function (tar, res, or) {
+    try {
+      return tar ? res : or
+    } catch (err) {
+      return err.message
+    }
+  })
+  env.addFilter('replace', function (tar) {
+    try {
+      if (isNumber(tar.charAt(0))) {
+        tar = '_' + tar
+      }
+      tar = tar.split(/\-/g).join('_')
+      return tar
+    } catch (err) {
+      return err.message
+    }
+  })
+
+  // TODO 兼容多path参数
+  env.addFilter('routePar', function (tar) {
+    try {
+      if (isNumber(tar.charAt(0))) {
+        tar = '_' + tar
+      }
+      tar = tar.split(/\-/g).join('_')
+      return tar
+    } catch (err) {
+      return err.message
+    }
+  })
+
+  return env
 }
 
-export function fuckinTar(env) {
-    return async function (temName, model, target) {
-        const text = env.render(temName + '.njk', model)
-        const p = convertPath(target)
-        await writeFile(path.resolve(p, ...model.dirName, model.fileName + '.js'), text)
-    }
+export function fuckinTar (env) {
+  return async function (temName, model, target) {
+    const text = env.render(temName + '.njk', model)
+    const p = convertPath(target)
+    await writeFile(
+      path.resolve(p, ...model.dirName, model.fileName + '.js'),
+      text
+    )
+  }
 }
--- src/render.js
+++ src/render.js
@@ -1,23 +1,25 @@
 /*
  * @Date: 2020-06-12 10:20:55
- * @LastEditors: songlin
- * @LastEditTime: 2020-06-12 11:33:08
+ * @LastEditors  : BillySong
+ * @LastEditTime : 2021-01-20 11:43:07
  * @FilePath: \codegen\src\render.js
  */
-import { createNjk, fuckinTar } from "./njk"
+import { createNjk, fuckinTar } from './njk'
 
 const render = fuckinTar(createNjk('../template', {}))
-export function renderIndex(dirName = [], model,target) {
-    Object.assign(model, {
-        dirName,
-        fileName: 'index'
-    })
-    render('index', model, target)
+export function renderIndex (dirName = [], model, target) {
+  Object.assign(model, {
+    dirName,
+    fileName: 'index'
+  })
+  console.log(model)
+
+  render('index', model, target)
 }
-export function renderModule(dirName = [], fileName, model,target) {
-    Object.assign(model, {
-        dirName,
-        fileName
-    })
-    render('module', model, target)
+export function renderModule (dirName = [], fileName, model, target) {
+  Object.assign(model, {
+    dirName,
+    fileName
+  })
+  render('module', model, target)
 }
